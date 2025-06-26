a24eaac44a354578e6f6dc2c15aa6274a3fd056a
Shereen Hamato <18262675+shereen-hamato@users.noreply.github.com>
2025-06-26 10:17:54 +0800

    Refactor entity classes to use auto-properties

--- Entity/Customer.cs
+++ Entity/Customer.cs
@@ -1,36 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Entity
 {
     public class Customer
     {
-        string custId;
-
-        public string CustId
-        {
-            get { return custId; }
-            set { custId = value; }
-        }
-
-        string name;
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        private string phnNumber;
-
-        public string PhnNumber
-        {
-            get { return phnNumber; }
-            set { phnNumber = value; }
-        }
-       
-
+        public string CustId { get; set; }
+        public string Name { get; set; }
+        public string PhnNumber { get; set; }
     }
 }
--- Entity/Employee.cs
+++ Entity/Employee.cs
@@ -1,48 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Entity
 {
     public class Employee
     {
-        string empId;
-
-        public string EmpId
-        {
-            get { return empId; }
-            set { empId = value; }
-        }
-
-        string name;
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        private string phnNumber;
-
-        public string PhnNumber
-        {
-            get { return phnNumber; }
-            set { phnNumber = value; }
-        }
-        private double salary;
-
-        public double Salary
-        {
-            get { return salary; }
-            set { salary = value; }
-        }
-        private string designation;
-
-        public string Designation
-        {
-            get { return designation; }
-            set { designation = value; }
-        }
+        public string EmpId { get; set; }
+        public string Name { get; set; }
+        public string PhnNumber { get; set; }
+        public double Salary { get; set; }
+        public string Designation { get; set; }
     }
 }
